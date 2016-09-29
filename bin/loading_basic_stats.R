3d185ba5fab23efafa7bedbee42775751949bb48
Erin Dickie <erin.dickie@camh.ca>
2016-09-29 12:14:40 -0400

    adding basic stats loading

---
+++ bin/loading_basic_stats.R
@@ -0,0 +1,3 @@
+ABIDE_fs5.3_thickness <- read.csv("../data/ABIDE_fs5.3_thickness.csv")
+ABIDE_fs5.3_LandRvolumes <- read.csv("../data/ABIDE_fs5.3_LandRvolumes.csv")
+ABIDE_Phenotype.csv <- read.csv("../data/ABIDE_fs5.3_LandRvolumes.csv")
